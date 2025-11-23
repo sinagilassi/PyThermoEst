b9cf676079e689eca9e161c0d6c46d85a5edbd85
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-11-23 14:17:08 -0500

    add joback_calc method

--- pyThermoEst/app.py
+++ pyThermoEst/app.py
@@ -1,17 +1,40 @@
 # import libs
-# local
-from .docs import Core
+import logging
+from typing import Dict
+# locals
 from .models import JobackGroupContributions
+from .core import Joback
 
+# NOTE: logger
+logger = logging.getLogger(__name__)
 
-def estimate():
-    print("This is the estimate function.")
-    CoreC = Core()
-    print(CoreC.ref_dict)
 
 # SECTION: Joback Group Contributions
 
+def joback_calc(groups: JobackGroupContributions | Dict[str, float]):
+    """
+    Using Joback method to calculate thermodynamic properties including
 
-def joback(groups: JobackGroupContributions):
-    print("This is the Joback groups function.")
-    print(groups)
+    Parameters
+    ----------
+    groups : JobackGroupContributions | Dict[str, float]
+        Group contributions for Joback method.
+
+    Returns
+    -------
+
+    """
+    try:
+        # SECTION: initialize Joback method
+        Joback_ = Joback(group_contributions=groups)
+
+        # NOTE: group contributions
+        groups_ = Joback_.group_contribution_idx
+
+        # SECTION: check group contributions
+        valid_groups_ = Joback_._check_group_contributions()
+
+        return groups_, valid_groups_
+    except Exception as e:
+        logger.error(f"Error in Joback calculation: {e}")
+        return None
