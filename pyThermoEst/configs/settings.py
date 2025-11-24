05a851654fd8109ebcf66342facb8f4ce3b10223
sina.gilassi <sina.gilassi@gmail.com>
2025-11-24 00:39:37 -0500

    add config for zr

--- pyThermoEst/configs/settings.py
+++ pyThermoEst/configs/settings.py
@@ -5,3 +5,10 @@ JOBACK_DATA_FILE = 'joback.csv'
 
 # NOTE: tables
 JOBACK_TABLE_COLUMN_GROUP = 'Group'
+
+# SECTION: zabransky-ruzicka method settings
+ZABRANSKY_RUZICKA_DATA_FILE_1 = 'zabransky-ruzicka-1.csv'
+ZABRANSKY_RUZICKA_DATA_FILE_2 = 'zabransky-ruzicka-2.csv'
+
+# NOTE: tables
+ZABRANSKY_RUZICKA_TABLE_COLUMN_GROUP = 'Group'
