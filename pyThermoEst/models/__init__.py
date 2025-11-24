f94fb4c126b308d056b70eeedba726bb23f42103
sina.gilassi <sina.gilassi@gmail.com>
2025-11-24 00:39:01 -0500

    upd

--- pyThermoEst/models/__init__.py
+++ pyThermoEst/models/__init__.py
@@ -1,14 +1,22 @@
+# ref
+from .ref import GroupUnit
 # joback group contributions
 from .jb import (
     JobackGroupContributions,
-    GroupUnit,
     JobackGroupData,
     JobackHeatCapacity
-    )
+)
+# zabransky ruzicka
+from .zr import (
+    ZabranskyRuzickaGroupContributions,
+    ZabranskyRuzickaGroupContributionsCorrections
+)
 
 __all__ = [
     "JobackGroupContributions",
     "GroupUnit",
     "JobackGroupData",
     "JobackHeatCapacity",
+    "ZabranskyRuzickaGroupContributions",
+    "ZabranskyRuzickaGroupContributionsCorrections"
 ]
