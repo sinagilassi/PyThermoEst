eff2fd3365fc8fc4dc0980c6b158e46026041431
sina.gilassi <sina.gilassi@gmail.com>
2025-11-27 00:15:06 -0500

    test

--- examples/joback-exp-0.py
+++ examples/joback-exp-0.py
@@ -3,8 +3,9 @@ import pyThermoEst
 from pyThermoEst.docs.joback import (
     joback_group_contribution_info,
     joback_group_contribution_names,
-    joback_group_contribution_ids
-    )
+    joback_group_contribution_ids,
+    joback_group_contribution_category
+)
 from rich import print
 
 # version
@@ -21,3 +22,11 @@ print(group_names)
 # NOTE: get Joback group contribution IDs
 group_ids = joback_group_contribution_ids()
 print(group_ids)
+
+# NOTE: get Joback group contribution category
+group_category = joback_group_contribution_category()
+print(group_category)
+print(f"Total categories: {len(group_category)}")
+# each group length
+for cat, items in group_category.items():
+    print(f"Category: {cat}, Total items: {len(items)}")
