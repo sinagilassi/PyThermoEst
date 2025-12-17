b2b7c74408c1e4a1bc4c60f6f521d1d0ac930363
sina.gilassi <sina.gilassi@gmail.com>
2025-12-17 01:31:08 -0500

    Add examples for estimating Antoine coefficients with experimental data

---
+++ examples/antoine/antoine-1.py
@@ -0,0 +1,21 @@
+# import libs
+import os
+from rich import print
+from pyThermoEst.docs.antoine import (
+    estimate_coefficients_from_experimental_data,
+    AntoineFitResult,
+)
+
+# NOTE: experimental data file path
+DATA_FILE_PATH = os.path.join(os.path.dirname(__file__), "data-2.csv")
+print(f"[bold green]DATA_FILE_PATH:[/bold green] {DATA_FILE_PATH}")
+
+# NOTE: estimate Antoine coefficients from experimental data
+result: AntoineFitResult | None = estimate_coefficients_from_experimental_data(
+    experimental_data=DATA_FILE_PATH,
+)
+if result is None:
+    print("[bold red]Failed to estimate Antoine coefficients from experimental data.[/bold red]")
+else:
+    print("[bold green]Estimated Antoine coefficients from experimental data:[/bold green]")
+    print(result)
---
+++ examples/antoine/antoine-2.py
@@ -0,0 +1,57 @@
+# import libs
+import os
+from rich import print
+from pythermodb_settings.models import Temperature, Pressure
+from pyThermoEst.docs.antoine import (
+    estimate_coefficients,
+    AntoineFitResult,
+)
+
+# NOTE: experimental data file path
+# ! Temperature unit: K
+temperatures = [
+    298,
+    308,
+    318,
+    328,
+    338,
+    348,
+    358,
+    368,
+    378,
+    388,
+    398,
+    408,
+]
+# ! Pressure unit: Pa
+Pressures = [
+    3392.900018,
+    5738.327528,
+    9332.604721,
+    14657.31121,
+    22310.8695,
+    33018.32269,
+    47638.5333,
+    67168.53995,
+    92744.96477,
+    125642.5053,
+    167269.663,
+    219161.9542,
+]
+
+# >> temperature
+Ts = [Temperature(value=T, unit="K") for T in temperatures]
+# >> pressure
+Ps = [Pressure(value=P, unit="Pa") for P in Pressures]
+
+
+# NOTE: estimate Antoine coefficients
+result: AntoineFitResult | None = estimate_coefficients(
+    temperatures=Ts,
+    pressures=Ps,
+)
+if result is None:
+    print("[bold red]Failed to estimate Antoine coefficients.[/bold red]")
+else:
+    print("[bold green]Estimated Antoine coefficients:[/bold green]")
+    print(result)
