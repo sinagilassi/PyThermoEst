9a996ebbd081fba982c932749087bccd7bbc9e8e
sina.gilassi <sina.gilassi@gmail.com>
2025-12-17 12:42:17 -0500

    Enhance Antoine fitting example by adding vapor pressure calculation and plotting functionality

--- examples/antoine/antoine-2.py
+++ examples/antoine/antoine-2.py
@@ -6,7 +6,9 @@ from pythermodb_settings.models import Temperature, Pressure
 from pyThermoEst.docs.antoine import (
     estimate_coefficients,
     AntoineFitResult,
+    calc_vapor_pressure
 )
+import matplotlib.pyplot as plt
 # local
 from antoine_utils import plot_antoine_fit
 
@@ -27,7 +29,7 @@ temperatures = [
     408,
 ]
 # ! Pressure unit: Pa
-Pressures = [
+pressures = [
     3392.900018,
     5738.327528,
     9332.604721,
@@ -45,7 +47,7 @@ Pressures = [
 # >> temperature
 Ts = [Temperature(value=T, unit="K") for T in temperatures]
 # >> pressure
-Ps = [Pressure(value=P, unit="Pa") for P in Pressures]
+Ps = [Pressure(value=P, unit="Pa") for P in pressures]
 
 
 # NOTE: estimate Antoine coefficients
@@ -62,12 +64,57 @@ else:
 
 # NOTE: plot Antoine fit
 if result is not None:
+    # A
+    A = result.model_dump().get("A")
+    # B
+    B = result.model_dump().get("B")
+    # C
+    C = result.model_dump().get("C")
+    # base
+    base = result.model_dump().get("base", "log10")
+
+    print("\n[bold blue]Calculating vapor pressures using the Antoine equation...[/bold blue]")
+    print(f"A = {A}, B = {B}, C = {C}, base = {base}\n")
+
+    # check
+    if A is None or B is None or C is None:
+        print("[bold red]Invalid Antoine coefficients.[/bold red]")
+        raise ValueError("Invalid Antoine coefficients.")
+
+    calc_res = []
+    for item in Ts:
+        res_ = calc_vapor_pressure(
+            temperature=item,
+            A=A,
+            B=B,
+            C=C,
+            base=base,
+        )
+
+        if res_ is not None:
+            calc_res.append(res_.model_dump().get("value"))
+
+    print(calc_res)
+
+    # plot
+    plt.figure()
+    plt.scatter(temperatures, pressures, label="Experimental")
+    plt.plot(temperatures, calc_res, label="Antoine fit")
+    # plt.yscale("log")
+    plt.xlabel('T (K)')
+    plt.ylabel('Psat (Pa)')
+    plt.title("Antoine Fit: Psat(T)")
+    plt.legend()
+    plt.show()
+
+# NOTE: plot using utility function
+if result is not None:
     plot_antoine_fit(
         T_data=np.array(temperatures),
-        P_data=np.array(Pressures),
+        P_data=np.array(pressures),
         fit_report=result.model_dump(),
         T_unit="K",
-        p_unit="Pa",
+        p_data_unit="Pa",
         n_curve=200,
         show_residuals=True,
     )
